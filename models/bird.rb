b0683ba1f0f1468c787536c265bdca4aad1706b2
jeevan <jeevan@indix.com>
2016-07-30 09:41:31 +0530

    Added continent validator

--- models/bird.rb
+++ models/bird.rb
@@ -1,16 +1,37 @@
 require 'mongoid'
 
 class Bird
-
   include Mongoid::Document
-  include Mongoid::Timestamps
-
   field :name, type: String
   field :family, type: String
   field :continents, type: Array
-	field :visible, type: Boolean
-  field :added, type: String
+	field :visible, type: Boolean, :default => false
+  field :added, type: String, :default => DateTime.now.utc.to_date.strftime("%Y-%m-%d")
 
 	validates_presence_of :name, :family, :continents
+	validate :continent_validator, :on => :create
 
+	def as_json(*args)
+	   super.tap { |hash| hash["id"] = (hash.delete "_id").to_s }
+	end
+
+	private
+
+  def continent_validator
+  	return  unless self.errors.messages[:continents].nil?
+
+  	unless self.continents.is_a? Array
+  		self.errors.add(:continents, "Please pass only string array of continents")
+  		return
+  	end
+
+  	self.continents.each do |continent|
+  		unless continent.is_a? String
+	  		self.errors.add(:continents, "Please pass only string array of continents")
+	  		return
+	  	end
+	  end
+  end
 end
+
+
